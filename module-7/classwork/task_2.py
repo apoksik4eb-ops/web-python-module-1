fcc10668ae087ccc03f19cb4a002ad0d99bd3f29
Sergey <apoksik4eb@gmail.com>
2026-02-23 11:40:24 +0300

    feat: дополнил классную работу

--- module-7/classwork/task_2.py
+++ module-7/classwork/task_2.py
@@ -52,27 +52,65 @@ with open("access.log", "w", encoding="utf-8") as file:
    file.write("2026-02-02,Мария,EXIT\n")
    file.write("2026-02-02,Олег,ENTER")
 
-users_enter, users_exit, user_on_enter = {}, {}, {}
+stats, inside, daily_enters = {}, {}, {}
+errors = set()
 
 with open("access.log", "r", encoding="utf-8") as file:
    for line in file:
       date, user, action = line.strip().split(",")
+      stats.setdefault(user, {"ENTER": 0, "EXIT": 0})
+      inside.setdefault(user, False)
+      daily_enters.setdefault(date, 0)
 
-      users_enter.setdefault(user, 0)
       if action == "ENTER":
-         users_enter[user] = users_enter.get(user, 0) + 1
+         if inside[user]:
+            errors.add(user)
+         inside[user] = True
+         stats[user]["ENTER"] += 1
+         daily_enters[date] += 1
+      else:
+         if not inside[user]:
+            errors.add(user)
+         inside[user] = False
+         stats[user]["EXIT"] += 1
 
-      users_exit.setdefault(user, 0)
-      if action == "EXIT":
-         users_exit[user] = users_exit.get(user, 0) + 1
+      max_day = None
+      max_enters = 0
+      for date, count in daily_enters.items():
+         if count > max_enters:
+            max_day = date
+            max_enters = count
+
+with open("access_report.txt", "w", encoding="utf-8") as file:
+   for user, status in stats.items():
+      for action, count in status.items():
+         file.write(f"{action}: Сотрудник {user} - {count} раз.\n")
+   file.write("\n")
+   for user, status in inside.items():
+      if status == True:
+         file.write(f"Сотрудник {user} находится внутри здания.\n")
+   file.write("\n")
+   for user in errors:
+      file.write(f"Сотрудник {user} имеет ошибку доступа.\n")
+   file.write("\n")
+   for date, count in daily_enters.items():
+      file.write(f"Количество входов {date} - {count} раз.\n")
+   file.write("\n")
+   file.write(f"{max_day} было максимальное количество входов - {max_enters} раз\n")
+
+#       users_enter.setdefault(user, 0)
+#       if action == "ENTER":
+#          users_enter[user] = users_enter.get(user, 0) + 1
+
+#       users_exit.setdefault(user, 0)
+#       if action == "EXIT":
+#          users_exit[user] = users_exit.get(user, 0) + 1
       
-      user_on_enter.setdefault(user, True)
-      if action == "ENTER":
-         user_on_enter.setdefault(user, True)
-      elif action == "EXIT":
-         user_on_enter.setdefault(user, False)
-print(user_on_enter)
+#       user_on_enter.setdefault(user, True)
+#       if action == "ENTER":
+#          user_on_enter.setdefault(user, True)
+#       elif action == "EXIT":
+#          user_on_enter.setdefault(user, False)
+# print(user_on_enter)
 
-
-
-print(users_enter, users_exit, user_on_enter)
+# print(users_enter, users_exit, user_on_enter)
