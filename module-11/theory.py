b3255d6f0c80685d7556c4cacd96b55d7c21c3da
Sergey <apoksik4eb@gmail.com>
2026-04-01 20:47:51 +0300

    cw: решение задач по модулю 11 от 01.04.26

---
+++ module-11/theory.py
@@ -0,0 +1,31 @@
+import json
+import pickle
+
+product = {
+    "id": 1,
+    "name": "Product-1",
+    "price": 900
+}
+
+json_text = json.dumps(product, indent=2)
+restored_product = json.loads(json_text)
+
+with open("product.json", "w", encoding="utf-8") as f:
+    json.dump(product, f, indent=2)
+
+with open("product.json", "r", encoding="utf-8") as f:
+    loaded_product = json.load(f)
+
+print(loaded_product)
+
+data = pickle.dumps(product)
+restored_pkl_product = pickle.loads(data)
+print(restored_pkl_product)
+
+with open("product.pkl", "wb") as f:
+    pickle.dump(product, f)
+
+with open("product.pkl", "rb") as f:
+    loaded_product_pkl = pickle.load(f)
+
+print(loaded_product_pkl)
