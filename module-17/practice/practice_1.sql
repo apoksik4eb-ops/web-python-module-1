0bbfb8fb566dbe7052d3838fd43e0b2ae330636c
Sergey <apoksik4eb@gmail.com>
2026-05-20 20:43:33 +0300

    cw: Решение задач по модулю 17 от 20.05.26

--- module-17/practice/practice_1.sql
+++ module-17/practice/practice_1.sql
@@ -45,3 +45,82 @@ values
 	('Hiring Platform', 3, 300000, TRUE),
 	('Accointing Automation', 4, 500000, FALSE),
 	('Internal Chat', 1, 150000, TRUE);
+
+-- ----------------------------------------------
+
+select
+	name,
+	salary,
+	case
+		when salary >= 150000 then 'high'
+		when salary >= 100000 then 'middle'
+		else 'low'
+	end as salary_level
+from employees;
+
+-- ----------------------------------------------
+
+select
+	e.name as employee_name,
+	coalesce(d.name, 'Без отдела') as department_name
+from employees e
+left join departments d on e.department_id = d.id;
+
+-- ----------------------------------------------
+
+select
+	d.id,
+	d.name
+from departments d
+where exists (
+	select 1 from employees e
+	where e.department_id = d.id
+);
+
+-- ----------------------------------------------
+
+select
+	e.id,
+	e.name
+from employees e
+where exists (
+	select 1 from projects p
+	where p.employee_id = e.id
+);
+
+-- ----------------------------------------------
+
+select
+	name as project_name,
+	budget,
+	case	
+		when is_active = true then 'active'
+		else 'close'
+	end as project_status
+from projects;
+
+
+-- ----------------------------------------------
+
+select
+	e.name as employee_name,
+	COUNT(p.id) as projects_count
+from employees e
+left join projects p on p.employee_id = e.id
+group by e.id, e.name
+order by projects_count desc;
+
+-- ----------------------------------------------
+
+UPDATE projects
+SET budget = budget + 50000
+WHERE is_active = true
+returning
+	name as project_name,
+	budget as new_budget;
+	
+-- ----------------------------------------------
+
+delete from projects
+where is_active = false
+returning name;
